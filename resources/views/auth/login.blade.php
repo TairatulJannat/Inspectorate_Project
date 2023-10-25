1725f4300a320ce1eed004503181163dc713cb7a
Md Manjur Rahman <shuvo.eco15@gmail.com>
2023-10-25 22:56:32 +0600

    login background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,9 +40,9 @@
 <section>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xl-5"><img class="bg-img-cover bg-center" src="{{ asset('assets/backend/images/logo/bg_2.jpg') }}" alt="looginpage">
+            <div class="col-xl-6"><img class="bg-img-cover bg-center" src="{{ asset('assets/backend/images/logo/bg_3.jpg') }}" alt="looginpage">
             </div>
-            <div class="col-xl-7 p-0">
+            <div class="col-xl-6 p-0">
                 <div class="login-card">
 
                     <form class="theme-form login-form" action="{{ route('login') }}" method="post">
@@ -86,9 +86,6 @@
                             <button class="btn btn-primary btn-block" type="submit">Login</button>
                         </div>
 
-{{--                        <div class="login-social-title">--}}
-{{--                            <h5>Security Through Knowledge</h5>--}}
-{{--                        </div>--}}
 
 
                         <div class="form-group">
