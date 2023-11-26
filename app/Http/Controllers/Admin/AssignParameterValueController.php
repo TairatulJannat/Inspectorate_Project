79746c3346b4536c9343cf43a513af5398699ac5
manjur-97 <shuvo.eco15@gmail.com>
2023-11-26 15:07:27 +0600

    parameter

--- app/Http/Controllers/Admin/AssignParameterValueController.php
+++ app/Http/Controllers/Admin/AssignParameterValueController.php
@@ -127,9 +127,6 @@ class AssignParameterValueController extends Controller
      */
     public function show(Request $request)
     {
-
-        // dd('$request->all()');
-        dd($request->all());
         $customMessages = [
             'item-type-id.required' => 'Please select an Item Type.',
             'item-id.required' => 'Please select an Item.',
