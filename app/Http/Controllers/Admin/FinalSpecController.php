fb7e305e348d2e0953529e29f341a59c7540f899
Riajul Islam <ritewu2014@gmail.com>
2024-01-15 12:48:08 +0600

    Minor.

--- app/Http/Controllers/Admin/FinalSpecController.php
+++ app/Http/Controllers/Admin/FinalSpecController.php
@@ -24,14 +24,179 @@ use Yajra\DataTables\Facades\DataTables;
 
 class FinalSpecController extends Controller
 {
-    //
-
     public function index()
     {
+        $insp_id = Auth::user()->inspectorate_id;
+        $admin_id = Auth::user()->id;
+        $section_ids = AdminSection::where('admin_id', $admin_id)->pluck('sec_id')->toArray();
+        $designation_id = AdminSection::where('admin_id', $admin_id)->pluck('desig_id')->first();
+        $desig_position = Designation::where('id', $designation_id)->first();
 
+        if ($designation_id == 1 || $designation_id == 0) {
+            $finalSpecNew = FinalSpec::where('status', 0)->count();
+            $finalSpecOnProcess = '0';
+            $finalSpecCompleted = '0';
+            $finalSpecDispatch = DocumentTrack::where('doc_type_id', 6)
+                ->leftJoin('final_specs', 'document_tracks.doc_ref_id', '=', 'final_specs.id')
+                ->where('reciever_desig_id', $designation_id)
+                ->where('track_status', 4)
+                ->where('final_specs.status', 4)
+                ->whereIn('document_tracks.section_id', $section_ids)
+                ->count();
+        } else {
 
+            $finalSpecNew = DocumentTrack::where('doc_type_id', 6)
+                ->leftJoin('final_specs', 'document_tracks.doc_ref_id', '=', 'final_specs.id')
+                ->where('reciever_desig_id', $designation_id)
+                ->where('track_status', 1)
+                ->where('final_specs.status', 0)
+                ->whereIn('document_tracks.section_id', $section_ids)
+                ->count();
 
-        return view('backend.finalSpec.finalSpec_incomming_new.index');
+            $finalSpecOnProcess = DocumentTrack::where('doc_type_id', 6)
+                ->leftJoin('final_specs', 'document_tracks.doc_ref_id', '=', 'final_specs.id')
+                ->where('reciever_desig_id', $designation_id)
+                ->where('track_status', 3)
+                ->where('final_specs.status', 3)
+                ->whereIn('document_tracks.section_id', $section_ids)
+                ->count();
+
+            $finalSpecCompleted = DocumentTrack::where('doc_type_id', 6)
+                ->leftJoin('final_specs', 'document_tracks.doc_ref_id', '=', 'final_specs.id')
+                ->where('reciever_desig_id', $designation_id)
+                ->where('track_status', 2)
+                ->where('final_specs.status', 1)
+                ->whereIn('document_tracks.section_id', $section_ids)
+                ->count();
+
+            $finalSpecDispatch = DocumentTrack::where('doc_type_id', 6)
+                ->leftJoin('final_specs', 'document_tracks.doc_ref_id', '=', 'final_specs.id')
+                ->where('reciever_desig_id', $designation_id)
+                ->where('track_status', 4)
+                ->where('final_specs.status', 4)
+                ->whereIn('document_tracks.section_id', $section_ids)
+                ->count();
+        }
+
+        return view('backend.finalSpec.finalSpec_incomming_new.index', compact('finalSpecNew', 'finalSpecOnProcess', 'finalSpecCompleted', 'finalSpecDispatch'));
+    }
+
+    public function all_data(Request $request)
+    {
+        // dd($request->all());
+        if ($request->ajax()) {
+
+            $insp_id = Auth::user()->inspectorate_id;
+            $admin_id = Auth::user()->id;
+            $section_ids = AdminSection::where('admin_id', $admin_id)->pluck('sec_id')->toArray();
+            $designation_id = AdminSection::where('admin_id', $admin_id)->pluck('desig_id')->first();
+            $desig_position = Designation::where('id', $designation_id)->first();
+
+            if (Auth::user()->id == 92) {
+                $query = FinalSpec::leftJoin('item_types', 'final_specs.item_type_id', '=', 'item_types.id')
+                    ->leftJoin('dte_managments', 'final_specs.sender', '=', 'dte_managments.id')
+                    ->leftJoin('sections', 'final_specs.sec_id', '=', 'sections.id')
+                    ->where('final_specs.status', 0)
+                    ->select('final_specs.*', 'item_types.name as item_type_name', 'final_specs.*', 'dte_managments.name as dte_managment_name', 'sections.name as section_name')
+                    ->get();
+            } elseif ($desig_position->id == 1) {
+
+                $query = FinalSpec::leftJoin('item_types', 'final_specs.item_type_id', '=', 'item_types.id')
+                    ->leftJoin('dte_managments', 'final_specs.sender', '=', 'dte_managments.id')
+                    ->leftJoin('sections', 'final_specs.sec_id', '=', 'sections.id')
+                    ->select('final_specs.*', 'item_types.name as item_type_name', 'final_specs.*', 'dte_managments.name as dte_managment_name', 'sections.name as section_name')
+                    ->where('final_specs.status', 0)
+                    ->get();
+            } else {
+
+                $FinalSpecs = FinalSpec::leftJoin('document_tracks', 'final_specs.id', '=', 'document_tracks.doc_ref_id')
+                    ->where('document_tracks.reciever_desig_id', $designation_id)
+                    ->where('final_specs.insp_id', $insp_id)
+                    ->where('final_specs.status', 0)
+                    ->whereIn('final_specs.sec_id', $section_ids)->pluck('final_specs.id', 'final_specs.id')->toArray();
+
+                $query = FinalSpec::leftJoin('item_types', 'final_specs.item_type_id', '=', 'item_types.id')
+                    ->leftJoin('dte_managments', 'final_specs.sender', '=', 'dte_managments.id')
+                    ->leftJoin('sections', 'final_specs.sec_id', '=', 'sections.id')
+                    ->select('final_specs.*', 'item_types.name as item_type_name', 'final_specs.*', 'dte_managments.name as dte_managment_name', 'sections.name as section_name')
+                    ->whereIn('final_specs.id', $FinalSpecs)
+                    ->where('final_specs.status', 0)
+                    ->get();
+
+                //......Start for DataTable Forward and Details btn change
+                $FinalSpecId = [];
+                if ($query) {
+                    foreach ($query as $Finalspec) {
+                        array_push($FinalSpecId, $Finalspec->id);
+                    }
+                }
+
+                $document_tracks_receiver_id = DocumentTrack::whereIn('doc_ref_id', $FinalSpecId)
+                    ->where('reciever_desig_id', $designation_id)
+                    ->first();
+
+                if (!$document_tracks_receiver_id) {
+                    $query = FinalSpec::where('id', 'no data')->get();
+                }
+                //......End for showing data for receiver designation
+            }
+            // $query->orderBy('id', 'asc');
+
+            return DataTables::of($query)
+                ->setTotalRecords($query->count())
+                ->addIndexColumn()
+
+                ->addColumn('status', function ($data) {
+                    if ($data->status == '0') {
+                        return '<button class="btn btn-success btn-sm">New</button>';
+                    } else {
+                        return '<button class="btn btn-success btn-sm">None</button>';
+                    }
+                })
+                ->addColumn('action', function ($data) {
+
+                    $DocumentTrack = DocumentTrack::where('doc_ref_id', $data->id)->where('doc_ref_id', 6)->latest()->first();
+                    $designation_id = AdminSection::where('admin_id', Auth::user()->id)->pluck('desig_id')->first();
+                    // dd($DocumentTrack);
+                    if ($DocumentTrack) {
+                        if ($designation_id  ==  $DocumentTrack->reciever_desig_id) {
+                            $actionBtn = '<div class="btn-group" role="group">';
+                            if ($designation_id == 3) {
+                                $actionBtn .= '<a href="' . url('admin/finalSpec/edit/' . $data->id) . '" class="edit2 ">Update</a>';
+                            }
+
+                            $actionBtn .= '<a href="' . url('admin/finalspec/details/' . $data->id) . '" class="edit">Forward</a>
+                            </div>';
+                        } else {
+                            $actionBtn = '<div class="btn-group" role="group">';
+                            if ($designation_id == 3) {
+                                $actionBtn .= '<a href="' . url('admin/finalSpec/edit/' . $data->id) . '" class="edit2 ">Update</a>';
+                            }
+                            $actionBtn .= '<a href="' . url('admin/finalspec/details/' . $data->id) . '" class="update">Forwarded</a>
+                            </div>';
+                        }
+
+                        if ($designation_id  ==  $DocumentTrack->sender_designation_id) {
+                            $actionBtn = '<div class="btn-group" role="group">';
+                            if ($designation_id == 3) {
+                                $actionBtn .= '<a href="' . url('admin/finalSpec/edit/' . $data->id) . '" class="edit2 ">Update</a>';
+                            }
+                            $actionBtn .= '<a href="' . url('admin/finalspec/details/' . $data->id) . '" class="update">Forwarded</a>
+                            </div>';
+                        }
+                    } else {
+                        $actionBtn = '<div class="btn-group" role="group">';
+                        if ($designation_id == 3) {
+                            $actionBtn .= '<a href="' . url('admin/finalSpec/edit/' . $data->id) . '" class="edit2 ">Update</a>';
+                        }
+                        $actionBtn .= ' <a href="' . url('admin/finalspec/details/' . $data->id) . '" class="edit">Forward</a>
+                        </div>';
+                    }
+                    return $actionBtn;
+                })
+                ->rawColumns(['action', 'status'])
+                ->make(true);
+        }
     }
 
     public function create()
@@ -76,13 +241,11 @@ class FinalSpecController extends Controller
         $data->final_spec_receive_Ltr_dt = $request->final_spec_receive_Ltr_dt;
         $data->fin_year_id = $request->fin_year_id;
 
-
         $data->received_by = Auth::user()->id;
         $data->remark = $request->remark;
         $data->status = 0;
         $data->created_at = Carbon::now()->format('Y-m-d');
         $data->updated_at = Carbon::now()->format('Y-m-d');;
-
 
         $data->save();
 
@@ -104,7 +267,6 @@ class FinalSpecController extends Controller
         $offer_reference_numbers = Offer::all();
         return view('backend.finalSpec.finalSpec_incomming_new.edit', compact('finalspec', 'item', 'dte_managments',  'item_types', 'fin_years', 'tender_reference_numbers', 'indent_reference_numbers', 'suppliers', 'offer_reference_numbers'));
     }
-
 
     public function update(Request $request)
     {
@@ -131,7 +293,6 @@ class FinalSpecController extends Controller
         $data->created_at = Carbon::now()->format('Y-m-d');
         $data->updated_at = Carbon::now()->format('Y-m-d');
 
-
         $data->save();
 
         return response()->json(['success' => 'Done']);
@@ -139,7 +300,6 @@ class FinalSpecController extends Controller
 
     public function details($id)
     {
-
         $details = FinalSpec::leftJoin('item_types', 'final_specs.item_type_id', '=', 'item_types.id')
             ->leftJoin('dte_managments', 'final_specs.sender', '=', 'dte_managments.id')
             ->leftJoin('fin_years', 'final_specs.fin_year_id', '=', 'fin_years.id')
@@ -155,9 +315,6 @@ class FinalSpecController extends Controller
             ->where('final_specs.id', $id)
             ->first();
 
-
-
-
         //     $details->additional_documents = json_decode($details->additional_documents, true);
         //         $additional_documents_names = [];
 
@@ -170,7 +327,6 @@ class FinalSpecController extends Controller
 
         // }
 
-
         $details->suppliers = json_decode($details->supplier_id, true);
 
         $supplier_names_names = [];
@@ -181,9 +337,6 @@ class FinalSpecController extends Controller
                 array_push($supplier_names_names, $supplier_names);
             }
         }
-
-
-
 
         $designations = Designation::all();
         $admin_id = Auth::user()->id;
@@ -208,7 +361,6 @@ class FinalSpecController extends Controller
         }
 
         //Start close forward Status...
-
         $sender_designation_id = '';
         foreach ($document_tracks as $track) {
             if ($track->sender_designation_id === $desig_id) {
@@ -216,25 +368,19 @@ class FinalSpecController extends Controller
                 break;
             }
         }
-
         //End close forward Status...
-
-
 
         //Start blade forward on off section....
         $DocumentTrack_hidden = DocumentTrack::where('doc_ref_id',  $details->id)
             ->where('doc_type_id', 6)
             ->latest()->first();
-
         //End blade forward on off section....
-
 
         return view('backend.finalSpec.finalSpec_incomming_new.details', compact('details', 'designations', 'document_tracks', 'desig_id',  'auth_designation_id', 'sender_designation_id', 'DocumentTrack_hidden', 'supplier_names_names'));
     }
 
     public function finalSpecTracking(Request $request)
     {
-
         $ins_id = Auth::user()->inspectorate_id;
         $admin_id = Auth::user()->id;
         $section_ids = AdminSection::where('admin_id', $admin_id)->pluck('sec_id')->toArray();
@@ -262,7 +408,6 @@ class FinalSpecController extends Controller
         $data->updated_at =  Carbon::now('Asia/Dhaka');
         $data->save();
 
-
         if ($desig_position->position == 7) {
 
             $data = FinalSpec::find($doc_ref_id);
@@ -287,60 +432,22 @@ class FinalSpecController extends Controller
                 $value->save();
             }
         }
-
-
-
-
         return response()->json(['success' => 'Done']);
     }
     public function get_offer_details($offerReferenceNo)
     {
-        $offer = Offer::where('reference_no' ,$offerReferenceNo)->first();
-// dd($offer);
-        $item=Items::where('id' , $offer->item_id)->first();
-        $item_type=Item_type::where('id' , $offer->item_type_id)->first();
-        $tender_reference_no = Tender::where('reference_no',$offer->tender_reference_no)->first();
-        
-        $indent_reference_no = Indent::where('reference_no',$offer->indent_reference_no)->first();
-        
+        $offer = Offer::where('reference_no', $offerReferenceNo)->first();
+        // dd($offer);
+        $item = Items::where('id', $offer->item_id)->first();
+        $item_type = Item_type::where('id', $offer->item_type_id)->first();
+        $tender_reference_no = Tender::where('reference_no', $offer->tender_reference_no)->first();
+
+        $indent_reference_no = Indent::where('reference_no', $offer->indent_reference_no)->first();
+
         $offer->suppliers = json_decode($offer->supplier_id, true);
-     
-    
+
         $suppliers = Supplier::whereIn('id',  $offer->suppliers)->get();
-        
-        return response()->json(['item' =>$item, 'itemType' =>$item_type,'tenderReferenceNo'=>$tender_reference_no,'indentReferenceNo'=> $indent_reference_no,'suppliernames'=> $suppliers]);
 
-    }
-
-    public function edit($id)
-    {
-        $finalSpec = FinalSpec::find($id);
-        $admin_id = Auth::user()->id;
-        $inspectorate_id = Auth::user()->inspectorate_id;
-        $dte_managments = Dte_managment::where('status', 1)->get();
-        $additional_documnets = Additional_document::where('status', 1)->get();
-        $item_types = Item_type::where('status', 1)->where('inspectorate_id', $inspectorate_id)->get();
-        $item = Items::where('id', $finalSpec->item_id)->first();
-        $fin_years = FinancialYear::all();
-        $suppliers = Supplier::all();
-        $tender_reference_numbers = Tender::all();
-        $indent_reference_numbers = Indent::all();
-        return view('backend.finalSpec.finalSpec_incomming_new.edit', compact('finalSpec', 'item', 'dte_managments', 'additional_documnets', 'item_types', 'fin_years', 'tender_reference_numbers', 'indent_reference_numbers', 'suppliers'));
-    }
-
-    public function edit($id)
-    {
-        $finalSpec = FinalSpec::find($id);
-        $admin_id = Auth::user()->id;
-        $inspectorate_id = Auth::user()->inspectorate_id;
-        $dte_managments = Dte_managment::where('status', 1)->get();
-        $additional_documnets = Additional_document::where('status', 1)->get();
-        $item_types = Item_type::where('status', 1)->where('inspectorate_id', $inspectorate_id)->get();
-        $item = Items::where('id', $finalSpec->item_id)->first();
-        $fin_years = FinancialYear::all();
-        $suppliers = Supplier::all();
-        $tender_reference_numbers = Tender::all();
-        $indent_reference_numbers = Indent::all();
-        return view('backend.finalSpec.finalSpec_incomming_new.edit', compact('finalSpec', 'item', 'dte_managments', 'additional_documnets', 'item_types', 'fin_years', 'tender_reference_numbers', 'indent_reference_numbers', 'suppliers'));
+        return response()->json(['item' => $item, 'itemType' => $item_type, 'tenderReferenceNo' => $tender_reference_no, 'indentReferenceNo' => $indent_reference_no, 'suppliernames' => $suppliers]);
     }
 }
