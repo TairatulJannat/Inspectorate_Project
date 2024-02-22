704d51c2a968fc8abf58cce920e8a46ab6be07d7
Md Manjur Rahman <136876574+manjur-97@users.noreply.github.com>
2024-02-22 14:25:54 +0600

    inote

---
+++ resources/views/backend/inote/all_inote_letter/daviation_create.blade.php
@@ -0,0 +1,25 @@
+<div class="inote_boc">
+    <form action="" id='saveDaviation' method="POST" enctype="multipart/form-data">
+        <div class="container">
+            <div>APPLICATION FOR DEVIATION</div>
+            <div class="d-flex justify-content-between">
+
+                <div class="d-flex">
+                    <div >1</div>
+                    <div>
+                        <p> a. Nomenclature</p>
+                    </div>
+
+                </div>
+                <div>
+                    File no:
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-box d-flex justify-content-between pt-2">
+            <button type="submit" class="btn btn-primary" id="form_submission_button">Previous</button>
+            <button type="submit" class="btn btn-primary" id="form_submission_button">Save And Next</button>
+        </div>
+    </form>
+</div>
